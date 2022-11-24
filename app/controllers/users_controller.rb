a7474214bdd70cb35939484f5c51e69ed8c70375
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-11-24 11:50:55 +0300

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+
+    # private
+
+    # def permitted_params
+    #     params.permit(:username, :city)
+    # end
+
 end
